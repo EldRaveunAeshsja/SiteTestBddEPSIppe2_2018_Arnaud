2b959e2b967834e6e62bd04da8f11bc8a047d7c5
Arnaud Gonin <arnaud.gonin@live.fr>
2018-03-13 15:34:09 +0100

    Initiation à Bootstrap

---
+++ main.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Site test BDD BTS</title>
+	<link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.css">
+	<link rel="stylesheet" type="text/css" href="tuto.css">
+
+</head>
+<body>
+	<div class="row">
+  	<div class="col-md-1">Largeur 4</div>
+  	<div class="col-md-1">Largeur 8</div>
+  	<div class="col-md-1">Largeur 4</div>
+  	<div class="col-md-1">Largeur 8</div>
+  	<div class="col-md-1">Largeur 4</div>
+  	<div class="col-md-1">Largeur 8</div>
+  	<div class="col-md-1">Largeur 4</div>
+  	<div class="col-md-1">Largeur 8</div>
+</div>
+</body>
+</html>
